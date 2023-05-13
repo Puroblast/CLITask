49eee5e99e23580dcca67450b3a9a29b60d221cb
Puroblast <Puroblast97@mail.ru>
2023-05-14 00:03:03 +0300

    refact. clean up

--- main.py
+++ main.py
@@ -6,40 +6,40 @@ from definitions import *
 list_of_needed_tasks = []
 
 
-def find_task_dependencies(tasks, task_name):
-    for concrete_task in tasks:
+def find_task_dependencies(all_tasks, task_name):
+    for concrete_task in all_tasks:
         if concrete_task['name'] == task_name:
             if not concrete_task['dependencies']:
                 list_of_needed_tasks.append(concrete_task['name'])
             else:
                 for dependency_name in concrete_task['dependencies']:
-                    find_task_dependencies(tasks, dependency_name)
+                    find_task_dependencies(all_tasks, dependency_name)
                 list_of_needed_tasks.append(concrete_task['name'])
 
 
-def find_build(builds):
+def find_build(all_tasks, all_builds):
     build_exist = False
     print("Build info:")
-    for build in builds:
-        if build['name'] == args['<name>']:
+    for concrete_build in all_builds:
+        if concrete_build['name'] == args['<name>']:
             build_exist = True
-            print(f" name: {build['name']}")
-            for task_name in build['tasks']:
-                find_task_dependencies(tasks, task_name)
+            print(f" name: {concrete_build['name']}")
+            for task_name in concrete_build['tasks']:
+                find_task_dependencies(all_tasks, task_name)
             print(" tasks:")
-            for task in list_of_needed_tasks:
-                print(f"  {task}")
+            for task_name in list_of_needed_tasks:
+                print(f"  {task_name}")
     if not build_exist:
         print(f"No such build, try 'python {sys.argv[0]} builds' to see list of available builds")
 
 
-def find_task(tasks):
+def find_task(all_tasks):
     task_exist = False
     print("Task info:")
-    for task in tasks:
-        if task['name'] == args['<name>']:
+    for concrete_task in all_tasks:
+        if concrete_task['name'] == args['<name>']:
             task_exist = True
-            print(f" name: {task['name']}\n dependencies: {','.join(task['dependencies'])}")
+            print(f" name: {concrete_task['name']}\n dependencies: {','.join(concrete_task['dependencies'])}")
     if not task_exist:
         print(f"No such task, try 'python {sys.argv[0]} tasks' to see list of available tasks")
 
@@ -71,7 +71,7 @@ if __name__ == '__main__':
             print(f" {build['name']}")
     if args['get']:
         if args['build']:
-            find_build(builds)
+            find_build(tasks, builds)
 
         if args['task']:
             find_task(tasks)
